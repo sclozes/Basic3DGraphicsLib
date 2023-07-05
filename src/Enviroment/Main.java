5ca8584bf2036037250fb12b3c01ad22b0064042
TheCool73 <gil100.stein@gmail.com>
2023-07-05 15:27:08 +0300

    lo rotse2

--- src/Enviroment/Main.java
+++ src/Enviroment/Main.java
@@ -2,12 +2,7 @@ package Enviroment;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        System.out.println("Good Morning SAUSAGE");
-
-
-        System.out.println("gal is Gal!");
 
     }
 }
--- src/Enviroment/Window.java
+++ src/Enviroment/Window.java
@@ -1,4 +1,22 @@
 package Enviroment;
 
-public class Window {
+import javax.swing.*;
+
+public class Window extends JPanel {
+
+
+    private int w,h;
+    private String title;
+
+    public Window(int Width, int Height, String Title) {
+
+        this.w = Width;
+        this.h = Height;
+        this.title = Title;
+
+
+    }
+
+
+
 }
