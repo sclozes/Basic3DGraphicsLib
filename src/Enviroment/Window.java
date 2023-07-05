2b913ab220c518a3aa4c86d5c4557ce81831b696
TheCool73 <gil100.stein@gmail.com>
2023-07-05 15:33:23 +0300

    lo rotse2

--- src/Enviroment/Window.java
+++ src/Enviroment/Window.java
@@ -1,20 +1,46 @@
 package Enviroment;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JPanel {
 
-
     private int w,h;
-    private String title;
+    private String t;
+    private Color c;
+    private JFrame f;
 
-    public Window(int Width, int Height, String Title) {
+    public Window(int Width, int Height, Color background, String title) {
 
         this.w = Width;
         this.h = Height;
-        this.title = Title;
+        this.t = title;
+        this.c = background;
+
+        JFrame frame = new JFrame(t);
+
+        this.setBackground(c);
+
+        this.f = frame;
+
+        f.setSize(w, h);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //f.getContentPane().setBackground(c);
+        f.setVisible(true);
+
+        f.add(this);
 
 
+    }
+
+    public void waitInSeconds(double seconds) {
+
+        try {
+            long milliseconds = Math.round(seconds * 1000); // Convert seconds to milliseconds
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            // Handle the exception (if needed)
+        }
     }
 
 
