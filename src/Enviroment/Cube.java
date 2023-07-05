a8d9ac966f1cabfd10695c9a8fd652dbcdbf1eee
TheCool73 <gil100.stein@gmail.com>
2023-07-05 16:34:17 +0300

    added the cube class

---
+++ src/Enviroment/Cube.java
@@ -0,0 +1,13 @@
+package Enviroment;
+
+import java.awt.*;
+
+public class Cube extends Shape{
+
+    public Cube(Location location, float width, float height, float depth, Color color) {
+
+
+
+    }
+
+}
--- src/Enviroment/Shape.java
+++ src/Enviroment/Shape.java
@@ -2,7 +2,10 @@ package Enviroment;
 
 public abstract class Shape {
 
-
+    private float[] x;
+    private float[] y;
+    private float[] z;
+    private java.util.List<Point> pointList = new java.util.ArrayList<>();
 
 
 
