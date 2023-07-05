630fac870ffa200a64a200f1475750b9c5be1d79
Itay Zukin <sclozz312@gmail.com>
2023-07-05 18:04:41 +0300

    Added Location

---
+++ src/Enviroment/Pyramid.java
@@ -0,0 +1,23 @@
+package Enviroment;
+
+import java.awt.*;
+
+public class Pyramid extends Shape{
+
+    public Pyramid(Window window, Location location, float width, float height, float depth, Color color) {
+
+        this.loc = location;
+        this.w = window;
+        this.c = color;
+
+        // Point at 0,0 of objects cords
+        pointList.add(new Point(w, loc,c));
+
+        Location cc = new Location()
+
+
+
+    }
+
+
+}
