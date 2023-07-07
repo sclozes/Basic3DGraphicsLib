d84d98ada3a852cea97209f2b4173dc0fd21d388
TheCool73 <gil100.stein@gmail.com>
2023-07-07 22:22:53 +0300

    added the pos array in the shape class

--- src/Enviroment/Shape.java
+++ src/Enviroment/Shape.java
@@ -7,6 +7,7 @@ public abstract class Shape {
     protected Window w;
     protected Color c;
     protected java.util.List<Point> pointList = new java.util.ArrayList<>();
+    protected Location[] pos;//added the pos array that represents the position of each point relative to the shape's location
 
     public java.util.List<Point> getPoints() {
         return pointList;
